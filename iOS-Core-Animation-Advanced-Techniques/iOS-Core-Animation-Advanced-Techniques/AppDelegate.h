3d0cbf1a457a839be9b7ac940c1ae4fb17524bb7
SteveJones <benkong_ah@foxmail.com>
2017-06-19 14:02:53 +0800

    commit project

---
+++ iOS-Core-Animation-Advanced-Techniques/iOS-Core-Animation-Advanced-Techniques/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS-Core-Animation-Advanced-Techniques
+//
+//  Created by 江奔 on 2017/6/19.
+//  Copyright © 2017年 江奔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
