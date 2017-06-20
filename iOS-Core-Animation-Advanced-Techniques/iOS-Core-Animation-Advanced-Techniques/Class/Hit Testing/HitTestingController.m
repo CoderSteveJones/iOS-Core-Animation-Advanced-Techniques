25c86ed6fc78a960aa2e3a90173ff5612e9fcbaa
SteveJones <benkong_ah@foxmail.com>
2017-06-20 13:35:23 +0800

    Hit testing coding

--- iOS-Core-Animation-Advanced-Techniques/iOS-Core-Animation-Advanced-Techniques/Class/Hit Testing/HitTestingController.m
+++ iOS-Core-Animation-Advanced-Techniques/iOS-Core-Animation-Advanced-Techniques/Class/Hit Testing/HitTestingController.m
@@ -26,19 +26,54 @@
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    
+//    [self containsPointTestWithTouches:touches];
+    
+    [self hitTestWithTouches:touches];
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+/**
+ containsPointTestWithTouches
+
+ @param touches touches
+ */
+- (void)containsPointTestWithTouches:(NSSet<UITouch *> *)touches
+{
+    // get touch position relative to main view
+    CGPoint point = [[touches anyObject] locationInView:self.view];
+    // conver point to the white layer`s coordinates
+    point = [self.layerView.layer convertPoint:point fromLayer:self.view.layer];
+    
+    if ([self.layerView.layer containsPoint:point]) {
+        point = [self.blueLayer convertPoint:point fromLayer:self.layerView.layer];
+        if ([self.blueLayer containsPoint:point]) {
+            NSLog(@"Inside blue layer");
+        }else{
+            NSLog(@"Inside orange layer");
+        }
+    }
+
 }
-*/
+
+
+/**
+ hitTestWithTouches
+
+ @param touches touches
+ */
+- (void)hitTestWithTouches:(NSSet<UITouch *> *)touches
+{
+    CGPoint point = [[touches anyObject] locationInView:self.view];
+    CALayer *layer = [self.layerView.layer hitTest:point];
+    
+    if (layer == self.blueLayer) {
+        NSLog(@"Inside blue layer");
+    }else if (layer == self.layerView.layer){
+        NSLog(@"Inside orange layer");
+    }
+}
 
 @end
